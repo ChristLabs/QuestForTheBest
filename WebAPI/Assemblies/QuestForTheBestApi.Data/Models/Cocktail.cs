c5ae1d76378831825df46f51d95651ae2bd5c880
Christian Armstrong <armstrongc@uspioneer.com>
2025-01-29 12:21:16 -0800

    Get cocktails

---
+++ WebAPI/Assemblies/QuestForTheBestApi.Data/Models/Cocktail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestForTheBestApi.Data.Models
+{
+    public class Cocktail
+    {
+        public short CocktailId { get; set; }
+
+        public string CocktailName { get; set; }
+
+        public string CocktailDescription { get; set; }
+
+        public DateTime DateOfQuest { get; set; }
+    }
+}
--- WebAPI/Assemblies/QuestForTheBestApi.Data/QuestForTheBestDataProcessor.cs
+++ WebAPI/Assemblies/QuestForTheBestApi.Data/QuestForTheBestDataProcessor.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Protocols;
 using System.Runtime.InteropServices;
 using QuestForTheBestApi.Data;
+using QuestForTheBestApi.Data.Models;
 
 namespace QuestForTheBestApi.Data
 {
@@ -11,21 +12,54 @@ namespace QuestForTheBestApi.Data
 		{
 			int ret = 0;
 			using (var sqlConn = new SqlConnection(QuestForTheBestApiDatabaseConnection.conn))
-			using (var cmd = sqlConn.CreateCommand())
 			{
-				cmd.CommandText = @"SELECT COUNT(*)
-										FROM Scores;";
-
-				sqlConn.Open();
-
-				using var dr = cmd.ExecuteReader();
-				if (dr.Read())
+				using (var cmd = sqlConn.CreateCommand())
 				{
-					ret = dr.GetInt32(0);
+					cmd.CommandText = @"SELECT COUNT(*)
+											FROM Scores;";
+
+					sqlConn.Open();
+
+					using var dr = cmd.ExecuteReader();
+					if (dr.Read())
+					{
+						ret = dr.GetInt32(0);
+					}
 				}
 			}
 
 			return ret;
-		}
-	}
+        }
+        public static List<Cocktail> GetCocktails()
+        {
+            var ret = new List<Cocktail>();
+
+            using (var sqlConn = new SqlConnection(QuestForTheBestApiDatabaseConnection.conn))
+            {
+                using (var cmd = sqlConn.CreateCommand())
+				{
+					cmd.CommandText = @"
+						SELECT CocktailName
+								,CocktailDescription
+								,DateOfQuest
+							FROM Cocktails;";
+
+					sqlConn.Open();
+
+					using var dr = cmd.ExecuteReader();
+					while (dr.Read())
+					{
+						ret.Add(new Cocktail()
+						{
+							CocktailName = dr.GetString(0),
+							CocktailDescription = dr.GetString(1),
+							DateOfQuest = dr.GetDateTime(2)
+						});
+					}
+				}
+			}
+
+            return ret;
+        }
+    }
 }

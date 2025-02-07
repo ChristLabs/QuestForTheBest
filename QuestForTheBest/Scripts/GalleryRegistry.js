f9dfffa6b772577a5d84081b4f70da56690ace45
Christian Armstrong <c.armstrong070@gmail.com>
2025-02-06 20:59:42 -0800

    Gallery modal enhancements

---
+++ QuestForTheBest/Scripts/GalleryRegistry.js
@@ -0,0 +1,66 @@
+var imageData = [
+    {
+        fileName: "Margarita_card.webp",
+        altText: "AI Margarita"
+    },
+    {
+        fileName: "MoscowMule_banner.webp",
+        altText: "AI Moscow Mule"
+    },
+    {
+        fileName: "MaiTai_card.webp",
+        altText: "AI Mai Tai"
+    },
+    {
+        fileName: "WhiskeySour_card.webp",
+        altText: "AI Whiskey Sour"
+    },
+    {
+        fileName: "Manhattan_card.webp",
+        altText: "AI Manhattan"
+    },
+    {
+        fileName: "HotToddy_banner.webp",
+        altText: "AI Hot Toddy"
+    },
+    {
+        fileName: "Clementines.jpg",
+        altText: "Cat Game"
+    },
+    {
+        fileName: "CockStats_banner.jpg",
+        altText: "AI Cock Stats"
+    },
+    {
+        fileName: "CocktailBanner.webp",
+        altText: "AI Cocks"
+    },
+    {
+        fileName: "manhattan1.jpg",
+        altText: "Pig Bar Photo From Manhattan Night"
+    },
+    {
+        fileName: "manhattan2.jpg",
+        altText: "Pig Bar Photo From Manhattan Night"
+    },
+    {
+        fileName: "manhattan4.jpg",
+        altText: "Pig Bar Photo From Manhattan Night"
+    },
+    {
+        fileName: "manhattan5.jpg",
+        altText: "COVE Photo From Manhattan Night"
+    },
+    {
+        fileName: "manhattan6.jpg",
+        altText: "COVE Waiter Took A Selfie"
+    },
+    {
+        fileName: "manhattan7.jpg",
+        altText: "COVE Group Photo From Manhattan Night"
+    },
+    {
+        fileName: "manhattan8.jpg",
+        altText: "COVE Group Photo From Manhattan Night"
+    }
+];
--- QuestForTheBest/Scripts/QuestForTheBest_Gallery.js
+++ QuestForTheBest/Scripts/QuestForTheBest_Gallery.js
@@ -1,28 +1,9 @@
-var imageUrls = [
-    "Margarita_card.webp",
-    "MoscowMule_banner.webp",
-    "MaiTai_card.webp",
-    "WhiskeySour_card.webp",
-    "Manhattan_card.webp",
-    "HotToddy_banner.webp",
-    "Clementines.jpg",
-    "CockStats_banner.jpg",
-    "CocktailBanner.webp",
-    "manhattan1.jpg",
-    "manhattan2.jpg",
-    "manhattan4.jpg",
-    "manhattan5.jpg",
-    "manhattan6.jpg",
-    "manhattan7.jpg",
-    "manhattan8.jpg",
-];
-
 function randomizeImageOrder(){
-    for (let i = imageUrls.length - 1; i > 0; i--) {
+    for (let i = imageData.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        const temp = imageUrls[i];
-        imageUrls[i] = imageUrls[j];
-        imageUrls[j] = temp;
+        const temp = imageData[i];
+        imageData[i] = imageData[j];
+        imageData[j] = temp;
     }
 }
 
@@ -44,10 +25,10 @@ function initMasonry(){
 function renderImages(){
     randomizeImageOrder();
 
-    imageUrls.forEach((image) => {
+    imageData.forEach((image) => {
         document.getElementById("imageContainer").insertAdjacentHTML("beforeend", `
             <div class="grid-item" onclick="showGalleryModal(event);">
-					<img src="Content/Images/Gallery/${image}" class="img-fluid" />
+					<img src="Content/Images/Gallery/${image.fileName}" alt="${image.altText}" class="img-fluid" />
             </div>`)
     });
 
@@ -58,8 +39,10 @@ renderImages();
 
 function showGalleryModal(event){
     var imageUrl = event.currentTarget.querySelector("img").src;
+    var altText = event.currentTarget.querySelector("img").alt;
 
     document.getElementById("galleryModalImg").src = imageUrl;
+    document.getElementById("galleryModalTitle").innerText = altText;
 
     var galleryModal = new bootstrap.Modal(document.getElementById("galleryModal"))
     galleryModal.show();

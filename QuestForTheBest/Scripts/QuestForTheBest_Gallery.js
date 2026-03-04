4e6d4ceeb9b242ebba11790164bdf3d7a50364b7
Christian Armstrong <armstrongc@uspioneer.com>
2026-03-03 16:07:54 -0800

    Fixed quotes breaking some gallery alt text

--- QuestForTheBest/Scripts/QuestForTheBest_Gallery.js
+++ QuestForTheBest/Scripts/QuestForTheBest_Gallery.js
@@ -28,8 +28,8 @@ function renderImages(){
     imageData.forEach((image) => {
         document.getElementById("imageContainer").insertAdjacentHTML("beforeend", `
             <div class="grid-item" onclick="showGalleryModal(event);">
-					<img src="Content/Images/Gallery/${image.subFolder}/${image.fileName}" alt="${image.altText}" class="img-fluid" />
-            </div>`)
+					<img src="Content/Images/Gallery/${image.subFolder}/${image.fileName}" alt="${image.altText.escapeHtml()}" class="img-fluid" />
+            </div>`);
     });
 
     initMasonry();
